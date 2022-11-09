3da8fa601b51c1f9ea120e786ac629512484f761
Tom <tomcat-26@hotmail.co.uk>
2022-11-09 21:58:18 +0800

    moved styles into individual folder, changed nav color, added text to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
+import '../styles/HomeStyles.css';
+
 export default function Home() {
     return (
       <div className='home--container'>
-        <h2>Home</h2>
+        <h2 className="home--title">Home</h2>
+        <h3>Welcome!</h3>
+        <p>This is a project made to practice React Router alongside React test-driven development principles. I hope you enjoy navigating this small app.</p>
       </div>
     );
   }
