8230929a601425e3f9411c8fa9119553f1488f49
Tom <tomcat-26@hotmail.co.uk>
2022-11-09 17:28:18 +0800

    Initial test for loading  passed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <nav className='nav'>
-        <h1 className="nav--title">App</h1>
+        <h1 className="nav--title">Shopping Cart App</h1>
         <Link to='/home' className="nav--link">Home</Link>
         <Link to='/shop' className="nav--link">Shop</Link>
       </nav>
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import RouteSwitch from './RouteSwitch';
 
+describe('tests for initial loading', () => {
+  it('Loads App', () => {
+    render(<RouteSwitch />)
+    const heading = screen.getByRole('heading')
+    expect(heading.textContent).toBe('Shopping Cart App')
+  });
+})
