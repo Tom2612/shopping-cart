0ab513c1fe9fb65905368c19cef8b7eaf7156699
Tom <tomcat-26@hotmail.co.uk>
2022-11-09 16:50:00 +0800

    Added React-router and set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Link, Outlet } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Link to='/home'>Home</Link>
+      <Link to='/shop'>Shop</Link>
+      <Outlet />
     </div>
   );
 }
---
+++ src/RouteSwitch.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+
+const RouteSwitch = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />}>
+                    <Route path='home' element={<Home />} />
+                    <Route path='shop' element={<Shop />} />
+                    <Route path='*' element={<p>Woops, nothing here!</p>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouteSwitch;
