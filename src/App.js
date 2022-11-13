6138e6708ecfa9c9f3027f15e00e992207d01cb1
Tom <tomcat-26@hotmail.co.uk>
2022-11-13 14:56:41 +0800

    Cleaned up Router, added routes into App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link, Outlet } from "react-router-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './styles/style.css';
+import Home from './components/Home';
+import Shop from './components/Shop';
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -14,19 +16,22 @@ function App() {
   }
 
   return (
-    <div>
+    <Router>
       <nav className='nav'>
         <h1 className="nav--title">Shopping Cart App</h1>
         <Link to='/home' className="nav--link">Home</Link>
         <Link to='/shop' className="nav--link">Shop</Link>
       </nav>
-      <main>
-        <div className='cart--info'>
+      <div className='cart--info'>
           <p>Cart ({cart.length} item)</p>
-        </div>
-        <Outlet addToCart={addToCart}/>
-      </main>
-    </div>
+      </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='home' element={<Home />} />
+        <Route path='shop' element={<Shop addToCart={addToCart} />} />
+        <Route path='*' element={<p>Woops, nothing here!</p>} />
+      </Routes>
+    </Router>
   );
 }
 
