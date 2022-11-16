dd405954a3157b8cab778458cbc32e9a9fec9a59
Tom <tomcat-26@hotmail.co.uk>
2022-11-16 15:58:19 +0800

    Added new tests for Cart functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
-import RouteSwitch from './RouteSwitch';
 
 describe('tests for initial loading', () => {
   it('Loads App', () => {
-    const { getByRole} = render(<RouteSwitch />)
+    const { getByRole} = render(<App />)
     const text = getByRole('heading', { level: 1 })
     expect(text.textContent).toBe('Shopping Cart App');
   });
 
   it('loads index route Home on initial load', () => {
-    const { getByRole } = render(<RouteSwitch />);
+    const { getByRole } = render(<App />);
     const text = getByRole('heading', { level: 2 });
     expect(text.textContent).toBe('Home');
   })
 
   it('Does not load another route on initial load', () => {
-    const { getByRole } = render(<RouteSwitch />);
+    const { getByRole } = render(<App />);
     const testText = getByRole('heading', { level: 2});
     expect(testText.textContent).not.toBe('Shop')
   });
 
   it('Changes display text when user changes route', () => {
-    const { getByRole } = render(<RouteSwitch />)
+    const { getByRole } = render(<App />)
     const link = screen.getByRole('link', {name: 'Shop'});
 
     const initialText = getByRole('heading', { level: 2 });
@@ -32,5 +31,46 @@ describe('tests for initial loading', () => {
 
     expect(initialText.textContent).toBe('Home')
     expect(finalText.textContent).toBe('Shop')
+  })
+
+  it('Displays item when accessing shop', () => {
+    const { getByRole } = render(<App />)
+    const link = screen.getByRole('link', { name: 'Shop'});
+
+    userEvent.click(link);
+    const itemText = screen.getByRole('heading', { name: 'Carrots'});
+    expect(itemText.textContent).toBe('Carrots');
+  })
+
+  it('adds 1 carrot to cart', () => {
+    render(<App />);
+    const link = screen.getByRole('link', { name: 'Shop'});
+    userEvent.click(link);
+
+    const qty = screen.getAllByText(/qty/i)[0];
+    const addBtn = screen.getAllByRole('button', { name: '+1'})[0];
+    userEvent.click(addBtn);
+    expect(qty.textContent).toBe('qty: 4')
+  })
+  it('decrement btn removes a carrot', () => {
+    render(<App />);
+    const link = screen.getByRole('link', { name: 'Shop'});
+    userEvent.click(link);
+
+    const qty = screen.getAllByText(/qty/i)[0];
+    const addBtn = screen.getAllByRole('button', { name: '-1'})[0];
+    userEvent.click(addBtn);
+    expect(qty.textContent).toBe('qty: 2')
+  });
+
+  it('adds new item to cart, increases cart length', () => {
+    render(<App />);
+    const link = screen.getByRole('link', { name: 'Shop'});
+    userEvent.click(link);
+
+    const qty = screen.getAllByText(/Cart/i)[1];
+    const addBtn = screen.getAllByRole('button', { name: 'Add to cart'})[2];
+    userEvent.click(addBtn);
+    expect(qty.textContent).toBe('Cart (3 items)')
   })
 })
