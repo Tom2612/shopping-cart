536aceb2058dd138fb51cbbbe36e635546d46d8a
Tom <tomcat-26@hotmail.co.uk>
2022-11-19 16:02:49 +0800

    Added img to each book

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -14,52 +14,62 @@ exports.fakeData = [
     {
         name: 'Dune',
         author: 'Frank Herbert',
-        price: 8.99
+        price: 8.99,
+        img: '../pictures/Dune.png'
     },
     {
         name: 'The Stand',
         author: 'Stephen King',
-        price: 8.99
+        price: 8.99,
+        img: '../pictures/The_Stand.png'
     },
     {
         name: 'Journey to the Center of the Earth',
         author: 'Jules Verne',
         price: 6.99,
+        img: '../pictures/JourneytotheCenter.png'
     },
     {
         name: 'The War of the Worlds',
         author: 'H.G. Wells',
-        price: 4.99
+        price: 4.99,
+        img: '../pictures/War_of_the_Worlds.png'
     },
     {
         name: 'The Time Machine',
         author: 'H.G. Wells',
-        price: 5.00
+        price: 5.00,
+        img: '../pictures/Time_Machine.png'
     },
     {
         name: 'Fahrenheit 451',
         author: 'Ray Bradbury',
-        price: 7.99
+        price: 7.99,
+        img: '../pictures/Fahrenheit 451.png'
     },
     {
         name: 'Do Androids Dream of Electric Sheep?',
         author: 'Phillip K. Dick',
-        price: 7.99
+        price: 7.99,
+        img: '../pictures/DADOES.png'
     },
     {
         name: 'The Handmaid\'s Tale',
         author: 'Margaret Atwood',
-        pride: 6.99
+        pride: 6.99,
+        img: '../pictures/Handmaids Take.png'
     },
     {
         name: 'Hyperion',
         author: 'Dan Simmons',
-        price: 8.30
+        price: 8.30,
+        img: '../pictures/Hyperion.png'
     },
     {
         name: 'Hitchhiker\'s Guide to the Galaxy',
         author: 'Douglas Adams',
-        price: 7.99
+        price: 7.99,
+        img: '../pictures/Hitchhikers Guide.png'
     },
     {
         name: 'Ender\'s Game',
@@ -84,7 +94,8 @@ exports.fakeData = [
     {
         name: 'Watchmen',
         author: 'Alan moore and David Gibbons',
-        price: 9.99
+        price: 9.99,
+        img: '../pictures/Watchmen.png'
     },
     {
         name: 'Jurassic Park',
