53e15f3b798d8558e236622ce28098a99c6e9c93
Tom <tomcat-26@hotmail.co.uk>
2022-11-19 12:59:34 +0800

    Changed data to books with authors

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -1,34 +1,102 @@
 exports.fakeData = [
     {
-        name: 'Carrots',
+        name: 'Frankenstein',
+        author: 'Mary Shelley',
         price: 0.7,
     },
     {
-        name: 'Potatoes',
+        name: '1984',
+        author: 'George Orwell',
         price: 0.6
     },
     {
-        name: 'Rice',
+        name: 'Dune',
+        author: 'Frank Herbert',
         price: 1.3
     },
     {
-        name: 'Tea',
+        name: 'The Stand',
+        author: 'Stephen King',
         price: 2.6
     },
     {
-        name: 'Brocolli',
+        name: 'Journey to the Center of the Earth',
+        author: 'Jules Verne',
         price: 0.8,
     },
     {
-        name: 'Peppers',
+        name: 'The War of the Worlds',
+        author: 'H.G. Wells',
         price: 0.5
     },
     {
-        name: 'Pasta',
+        name: 'The Time Machine',
+        author: 'H.G. Wells',
         price: 1.0
     },
     {
-        name: 'Tomatoes',
+        name: 'Fahrenheit 451',
+        author: 'Ray Bradbury',
         price: 1.3
+    },
+    {
+        name: 'Do Androids Dream of Electric Sheep?',
+        author: 'Phillip K. Dick',
+        price: 7.99
+    },
+    {
+    name: 'The Handmaid\'s Tale',
+        author: 'Margaret Atwood',
+        pride: 6.99
+    },
+    {
+        name: 'Hyperion',
+        author: 'Dan Simmons',
+        price: 8.30
+    },
+    {
+        name: 'Hitchhiker\'s Guide to the Galaxy',
+        author: 'Douglas Adams',
+        price: 7.99
+    },
+    {
+        name: 'Ender\'s Game',
+        author: 'Orson Scott Guard',
+        price: 5.99
+    },
+    {
+        name: 'Brave New World',
+        author: 'Aldous Huxley',
+        price: 5.50
+    },
+    {
+        name: 'I, Robot',
+        author: 'Isaac Asimov',
+        price: 6.50
+    },
+    {
+        name: '2001: A Space Odyssey',
+        author: 'Arthur C. Clarke',
+        price: 9.99
+    }, 
+    {
+        name: 'Watchmen',
+        author: 'Alan moore and David Gibbons',
+        price: 10.99
+    },
+    {
+        name: 'Jurassic Park',
+        author: 'Michael Crichton',
+        price: 6.99
+    },
+    {
+        name: 'Slaughterhouse-five',
+        author: 'Kurt Vonnegut',
+        price: 2.99
+    },
+    {
+        name: 'Neuromancer',
+        author: 'William Gibson',
+        price: 5.99
     }
 ]
