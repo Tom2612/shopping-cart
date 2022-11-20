3ca7963e4d8b741498ce76b4f7cc89b6d318c95a
Tom <tomcat-26@hotmail.co.uk>
2022-11-20 13:19:25 +0800

    updated homepage, added styles, created scifi lines

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
-import '../styles/HomeStyles.css';
+import '../styles/Home.css';
 
 export default function Home() {
     return (
       <div className='home--container'>
-        <h2 className="home--title">Home</h2>
-        <h3>Welcome!</h3>
-        <p>This is a project made to practice React Router alongside React test-driven development principles. I hope you enjoy navigating this small app.</p>
-        <img src="https://images.unsplash.com/photo-1667845210425-5f805be2f355?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="random-image"/>
+        <h2 className="home--title">Sci-fi Book Shop</h2>
+        <h3>May the force forever be in your favour</h3>
+        <p>This is a fake bookshop that sells only the top 20 Sci-fi books of all time according to a random website I found one day.</p>
+        {/* <img src="https://images.unsplash.com/photo-1568952433726-3896e3881c65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="random-image"/> */}
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
       </div>
     );
   }
