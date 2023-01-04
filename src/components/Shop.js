5cc438f91321157bc63061bf4a48c8a30fc7a999
Tom <tomcat-26@hotmail.co.uk>
2023-01-04 15:43:16 +0800

    Added cart controls into Shop.js

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,55 @@
 import Item from "./Item";
 import Cart from "./Cart";
 import '../styles/Shop.css'
+import { useState } from "react";
+import { fakeData } from "../utils/Data";
 
 export default function Shop(props) {
   const { cart, total } = props;
+  const [data, setData] = useState(fakeData);
+  // const [cart, setCart] = useState([]);
+  // const [total, setTotal] = useState(0);
+  const [qty, setQty] = useState([]);
+
+  // const addToCart = (name, price) => {
+  //   const exists = cart.find(item => item.name === name);
+  //   if(exists) {
+  //     return;
+  //   } else {
+  //     SpeechRecognitionAlternative((prev) => {
+  //       return [...prev, { name, price, quantity: 1}];
+  //     });
+  //   };
+  // };
+
+  // const handleChange = (name, e) => {
+  //   setCart((prev) => {
+  //     return prev.map(item => (item.name === name ? { ...item, quantity: e.target.value } : item))''
+  //   });
+  // };
+
+  // const increment = (name) => {
+  //   setCart((prev) => {
+  //     return prev.map(item => ( item.name === name ? {...item, quantity: item.quantity + 1 } : item));
+  //   });
+  // };
+
+  // const decrement = (name) => {
+  //   const foundItem = cart.find(item => item.name === name);
+  //   if (!foundItem){
+  //     console.log('no such item');
+  //     return;
+  //   } else if (foundItem.quantity === 1) {
+  //     const newCart = cart.filter(item => item !== foundItem);
+  //     setCart(newCart);
+  //   } else {
+  //     setCart(prev => {
+  //       return prev.map(item => (item.name === name ? {...item, quantity: item.quantity - 1} : item
+  //       ));
+  //     });
+  //   }
+  // }
+
     return (
       <div className='shop--container'>
         <div className='shop--info'>
