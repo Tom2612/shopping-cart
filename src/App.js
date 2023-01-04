6923200c3a8d2f78d904048fafb39732a3b1db1b
Tom <tomcat-26@hotmail.co.uk>
2023-01-04 15:54:47 +0800

    Added all shop functionality into Shop.js, cleaned App,js

--- src/App.js
+++ src/App.js
@@ -1,78 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import './styles/style.css';
 import Home from './components/Home';
 import Shop from './components/Shop';
-import {fakeData } from './utils/Data';
 import UserCart from './components/UserCart';
 import SignUp from './components/SignUp';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 
 function App() {
-  const [data, setData] = useState(fakeData)
-  const [cart, setCart] = useState([]);
-  const [total, setTotal] = useState(0);
-  const [qty, setQty] = useState([]);
-  // const { currentUser } = useAuth();
-
-  useEffect(() => {
-    setTotal(cart.map(item => item.quantity * item.price).reduce((a, b) => a + b, 0).toFixed(2));
-  }, [cart])
-
-  useEffect(() => {
-    setQty(cart.map(item => item.quantity).reduce((a, b) => a + b, 0));
-  }, [cart])
-
-  const addToCart = (name, price) => {
-    const exists = cart.find(item => item.name === name);
-    if (exists) {
-      console.log('already in basket!')
-    } else {
-      setCart((prev) => {
-        return [...prev, {name: name, price: price, quantity: 1 }]
-      });
-    }
-  };
-
-  const handleChange = (name, e) => {
-    setCart((prev) => {
-      return prev.map(item => (item.name === name ? {...item, quantity: e.target.value } : item))
-    })
-  }
-  
-  const increment = (name) => {
-    //This on works below
-
-    // const exists = cart.find((x) => x.name === name);
-    // if (exists) {
-    //   setCart(cart.map(item => item.name === name ? {...exists, quantity: exists.quantity + 1 } : item));
-    // } else {
-    //   setCart([...cart])
-    // }
-
-    //This one also works now
-    setCart((prev) => {
-      return prev.map(item => ( item.name === name ? {...item, quantity: item.quantity + 1 } : item ));
-    });
-  };
-
-  const decrement = (name) => {
-    const foundItem = cart.find(item => item.name === name);
-    if (!foundItem){
-      console.log('no such item');
-      return;
-    } else if (foundItem.quantity === 1) {
-      const newCart = cart.filter(item => item !== foundItem);
-      setCart(newCart);
-    } else {
-      setCart(prev => {
-        return prev.map(item => (item.name === name ? {...item, quantity: item.quantity - 1} : item
-        ));
-      });
-    }
-  }
-
+ 
   return (
     <HashRouter>
       <AuthProvider>
@@ -86,19 +23,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path='home' element={<Home />} />
-          <Route 
-            path='/shop' 
-            element={<Shop 
-              fakeData={data} 
-              addToCart={addToCart} 
-              increment={increment} 
-              decrement={decrement} 
-              cart={cart} 
-              handleChange={handleChange} 
-              total={total}
-              qty={qty}
-            />} 
-          />
+          <Route path='/shop' element={<Shop />} />
           <Route path='/cart' element={<UserCart />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='*' element={<p>Woops, nothing here!</p>} />
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,76 +1,80 @@
 import Item from "./Item";
 import Cart from "./Cart";
 import '../styles/Shop.css'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { fakeData } from "../utils/Data";
 
-export default function Shop(props) {
-  const { cart, total } = props;
+export default function Shop() {
   const [data, setData] = useState(fakeData);
-  // const [cart, setCart] = useState([]);
-  // const [total, setTotal] = useState(0);
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
   const [qty, setQty] = useState([]);
 
-  // const addToCart = (name, price) => {
-  //   const exists = cart.find(item => item.name === name);
-  //   if(exists) {
-  //     return;
-  //   } else {
-  //     SpeechRecognitionAlternative((prev) => {
-  //       return [...prev, { name, price, quantity: 1}];
-  //     });
-  //   };
-  // };
+  useEffect(() => {
+    setTotal(cart.map(item => item.quantity * item.price).reduce((a, b) => a + b, 0).toFixed(2));
+    setQty(cart.map(item => item.quantity).reduce((a, b) => a + b, 0));
+  }, [cart]);
 
-  // const handleChange = (name, e) => {
-  //   setCart((prev) => {
-  //     return prev.map(item => (item.name === name ? { ...item, quantity: e.target.value } : item))''
-  //   });
-  // };
+  const addToCart = (name, price) => {
+    const exists = cart.find(item => item.name === name);
+    if(exists) {
+      return;
+    } else {
+      setCart((prev) => {
+        return [...prev, { name, price, quantity: 1}];
+      });
+    };
+  };
 
-  // const increment = (name) => {
-  //   setCart((prev) => {
-  //     return prev.map(item => ( item.name === name ? {...item, quantity: item.quantity + 1 } : item));
-  //   });
-  // };
+  const handleChange = (name, e) => {
+    setCart((prev) => {
+      return prev.map(item => (item.name === name ? { ...item, quantity: e.target.value } : item));
+    });
+  };
 
-  // const decrement = (name) => {
-  //   const foundItem = cart.find(item => item.name === name);
-  //   if (!foundItem){
-  //     console.log('no such item');
-  //     return;
-  //   } else if (foundItem.quantity === 1) {
-  //     const newCart = cart.filter(item => item !== foundItem);
-  //     setCart(newCart);
-  //   } else {
-  //     setCart(prev => {
-  //       return prev.map(item => (item.name === name ? {...item, quantity: item.quantity - 1} : item
-  //       ));
-  //     });
-  //   }
-  // }
+  const increment = (name) => {
+    setCart((prev) => {
+      return prev.map(item => ( item.name === name ? {...item, quantity: item.quantity + 1 } : item));
+    });
+  };
+
+  const decrement = (name) => {
+    const foundItem = cart.find(item => item.name === name);
+    if (!foundItem){
+      console.log('no such item');
+      return;
+    } else if (foundItem.quantity === 1) {
+      const newCart = cart.filter(item => item !== foundItem);
+      setCart(newCart);
+    } else {
+      setCart(prev => {
+        return prev.map(item => (item.name === name ? {...item, quantity: item.quantity - 1} : item
+        ));
+      });
+    }
+  }
 
     return (
       <div className='shop--container'>
         <div className='shop--info'>
           <h2>Browse all titles</h2>
           <div>
-            {cart.length > 0 ? <Cart cart={cart} total={total} qty={props.qty}/> : <h3 className='cart--container cart--title'>Cart (0 Items)</h3>}
+            {cart.length > 0 ? <Cart cart={cart} total={total} qty={qty}/> : <h3 className='cart--container cart--title'>Cart (0 Items)</h3>}
           </div>
         </div>
         <div className='list--container'>
-          {props.fakeData.map(item => {
+          {data.map(item => {
             return <Item
                 key={item.name}
                 name={item.name} 
                 price={item.price}
                 author={item.author}
                 img={item.img}
-                addToCart={props.addToCart} 
-                increment={props.increment} 
-                decrement={props.decrement} 
-                cart={props.cart} 
-                handleChange={props.handleChange}
+                addToCart={addToCart} 
+                increment={increment} 
+                decrement={decrement} 
+                cart={cart} 
+                handleChange={handleChange}
               />
             })
           }
