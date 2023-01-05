83c889464eff9ab136956bc8ca268d52fa1a860c
Tom <tomcat-26@hotmail.co.uk>
2023-01-05 08:31:33 +0800

    Added PrivateRoute.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import UserCart from './components/UserCart';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './contexts/AuthContext';
 import SignIn from './components/SignIn';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
  
@@ -20,7 +21,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path='home' element={<Home />} />
           <Route path='/shop' element={<Shop />} />
-          <Route path='/cart' element={<UserCart />} />
+          <Route path='/cart' element={<PrivateRoute><UserCart /></PrivateRoute>} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/signin' element={<SignIn />} />
           <Route path='*' element={<p>Woops, nothing here!</p>} />
