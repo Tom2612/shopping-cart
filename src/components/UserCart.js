e5778ebda97b90daa0842e7ec51bbeb6ee2cf98f
Tom <tomcat-26@hotmail.co.uk>
2023-01-09 21:32:23 +0800

    Trying to get userCart to display

--- src/components/UserCart.js
+++ src/components/UserCart.js
@@ -1,26 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { db } from '../firebase';
 import { doc, getDoc } from 'firebase/firestore';
 
 export default function UserCart() {
   const { currentUser } = useAuth();
+  const [userCart, setUserCart] = useState([]);
+  const [loading, setLoading] = useState(false);
   
   useEffect(() => {
     const docSnap = async () => {
       const docRef = doc(db, 'users', currentUser.uid);
       const docs = await getDoc(docRef);
-      console.log(docs.data())
-      return docs;
+      setUserCart(docs.data().cart);
+
+      console.log(docs.data().cart);
     }
     docSnap();
+    setLoading(true);
     
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [userCart])
 
   return (
     <div className='user-cart--container'>
-      <h1>You can't see this!</h1>
-      {currentUser.email}
+      <h1>My Cart</h1>
+      <p>{currentUser.email}</p>
+      {!loading && userCart.map(item => {
+        <p>{item.name}</p>
+      })}
     </div>
   )
 }
