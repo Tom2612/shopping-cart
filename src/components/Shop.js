82ab7f6fb3bc4f6f474dbbf8900928a00dc2de01
Tom <tomcat-26@hotmail.co.uk>
2023-01-09 20:48:49 +0800

    Added function to add to server

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,7 +3,7 @@ import Cart from "./Cart";
 import '../styles/Shop.css'
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
-import { addDoc, collection, getDocs, setDoc } from 'firebase/firestore';
+import { addDoc, collection, getDocs, setDoc, doc } from 'firebase/firestore';
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 
@@ -35,19 +35,19 @@ export default function Shop() {
   useEffect(() => {
     setTotal(cart.map(item => item.quantity * item.price).reduce((a, b) => a + b, 0).toFixed(2));
     setQty(cart.map(item => item.quantity).reduce((a, b) => a + b, 0));
-    // uploadCart();
+    uploadCart();
   }, [cart]);
 
-  // const uploadCart = async () => {
-  //   try {
-  //     //Add doc to this
-  //     await setDoc(collection(db, 'users', currentUser.uid), {
-  //     cart
-  //   })
-  // }catch(e) {
-  //   console.log(e)
-  // }
-  // }
+  const uploadCart = async () => {
+    try {
+      //Add doc to this
+      await setDoc(doc(db, 'users', currentUser.uid), {
+      cart
+    })
+  }catch(e) {
+    console.log(e);
+  }
+  }
 
   const addToCart = (name, price) => {
     //Add authentication here - no user = no add to cart
