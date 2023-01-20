e6df6739d686645d673025e30d862562bdedada2
tom <tomcat-26@hotmail.co.uk>
2023-01-20 16:04:52 +0800

    CartContext

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -2,3 +2,19 @@ import React, { useContext, useState, useEffect } from 'react';
 
 const CartContext = React.createContext();
 
+// Custom Cart hook
+export function useCart() {
+    return useContext(CartContext);
+}
+
+export function CartProvider({ children }) {
+    const value = {
+        cart: true
+    }
+
+    return (
+        <CartContext.Provider value={value}>
+            { children }
+        </CartContext.Provider>
+    )
+}
